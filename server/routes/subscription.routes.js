077c97b9b3cb83a4dacca11f651b5a73e1b0f502
Dhanusha-Ravikannan <dhanushravi904@gmail.com>
2025-01-27 17:11:04 +0530

    subscription plan

--- server/routes/subscription.routes.js
+++ server/routes/subscription.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllSubscription, createSubscription, getSubscription, getSubscriptionNames } = require('../controllers/subscription.controllers');
+const { getAllSubscription, createSubscription, getSubscription, getSubscriptionNames,  } = require('../controllers/subscription.controllers');
 const router = express.Router();
 
 router.get('/getAllSub',getAllSubscription)
 router.get('/getSUB',getSubscription)
 router.get('/names',getSubscriptionNames)
 router.post('/createSub',createSubscription)
+// router.get("/1234",getNames)
 
 module.exports = router;
